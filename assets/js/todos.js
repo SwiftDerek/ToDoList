8286cb85fcaf24606cf966c5840f038537fd41ff
Derek Williams <souldishwasher@gmail.com>
2016-08-03 16:58:18 -0500

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+// Toggle To-Do List items to be completed or not completed
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// Click on Trash Can to delete To-Do
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
+
+// Add New Item to List Button
+$('.fa-plus').click(function () {
+   $('input[type="text"]').fadeToggle();
+});
